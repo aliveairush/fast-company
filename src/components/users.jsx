7521cab345560f73210bc5c06e0ab6ed3429b878
Aimurzin Ainur <aliveairush@gmail.com>
2022-03-25 18:53:17 +0300

    Added basic table styling by using bootstrap.

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from "../api";
 
 function Users() {
+
+  const [users, setUsers] = useState(api.users.fetchAll);
+
+  const handleDelete = (userId) => {
+
+  }
+
+  const renderPhrase = (number) => {
+
+  }
+
   return (
-    <h1>Users</h1>
+    <table className="table">
+      <thead>
+      <tr>
+        <th scope="col">Имя</th>
+        <th scope="col">Качества</th>
+        <th scope="col">Профессия</th>
+        <th scope="col">Встретился, раз</th>
+        <th scope="col">Оценка</th>
+        <th scope="col"/>
+      </tr>
+      </thead>
+      <tbody>
+      {users.map(user =>
+        <tr key={user._id} >
+          <td>{user.name}</td>
+          <td>{user.qualities.map(quality => <span key={quality._id} className={`badge bg-secondary bg-${quality.color}`}>{quality.name}</span>)}</td>
+          <td>{user.profession.name}</td>
+          <td>{user.completedMeetings}</td>
+          <td>{user.rate}</td>
+          <td><button className="btn btn-danger">Delete</button></td>
+        </tr>)}
+      </tbody>
+    </table>
   );
 }
 
