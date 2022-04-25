6ed9506aeca92740062a0ccf17ba73c712d0d3e5
ainur.aimurzin <aliveairush@gmail.com>
2022-04-25 04:58:23 +0300

    Refactoring code.

--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -1,21 +1,65 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Table from "./table";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
-const UsersTable = ({data, onSort, columns, selectedSort}) => {
+import QualitiesList from "./qualitiesList";
+import Bookmark from "./bookmark";
+import paginate from "../utils/paginate";
+import Pagination from "./pagination";
+import SearchStatus from "./searchStatus";
+const UsersTable = ({data, onSort, onDelete, selectedSort}) => {
 
+  const pageSize = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersCrop = paginate(data, currentPage, pageSize);
+
+  const handlePageChange = (pageIndex) => setCurrentPage(pageIndex);
+
+  // If every user on the page has been deleted and page is bigger than 0 then show previous page
+  if (!usersCrop.length && currentPage > 0) {
+    handlePageChange(currentPage - 1);
+  }
+
+
+  const columns = [
+    {property: "name", title: "Имя"},
+    {title: "Качества",
+      component: (user) =>  <QualitiesList qualities={user.qualities}/>
+    },
+    {property: "profession.name", title: "Профессия"},
+    {property: "completedMeetings", title: "Встретился, раз"},
+    {property: "rate", title: "Оценка"},
+    {property: "bookmark", title: "Избранное",
+      component: () => <Bookmark/>
+    },
+    {component: (user) =>
+      <button className="btn btn-danger"
+        onClick={() => onDelete(user._id)} >Delete
+      </button>}
+  ];
+    
   return (
-    <Table>
-      <TableHeader {...{onSort, selectedSort, columns}} />
-      <TableBody {...{columns, data}}/>
-    </Table>
+    <>
+      <SearchStatus length={data.length}/>
+      <Table>
+        <TableHeader {...{onSort, selectedSort, columns}} />
+        <TableBody columns={columns} data={usersCrop} />
+      </Table>
+
+      <Pagination
+        itemsCount={data.length}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
   );
 };
 
 UsersTable.propTypes = {
   data: PropTypes.array.isRequired,
-  columns: PropTypes.array.isRequired,
+  onDelete: PropTypes.func.isRequired,
   onSort: PropTypes.func,
   selectedSort: PropTypes.object,
 };
---
+++ src/forms/usersForm.jsx
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from "react";
+import UsersTable from "../components/usersTable";
+import GroupList from "../components/groupList";
+import api from "../api";
+import _ from "lodash";
+
+const UsersForm = () => {
+  
+  const [users, setUsers] = useState(api.users.fetchAll);
+
+  // Array of professions
+  const [professions, setProfessions] = useState();
+  
+  // string by which filtering will be proceeded
+  const [selectedProf, setSelectedProf] = useState();
+  // Object which defines sorting
+  const [sortBy, setSortBy] = useState({iter: "name", order: "asc"});
+
+  // Setting all users 
+  useEffect(() => {
+    api.professions.fetchAll()
+      .then(data => setProfessions(data));
+  });
+
+  // Filtering users by profession
+  const filteredUsers = selectedProf ? users.filter(user => user.profession._id === selectedProf._id) : users;
+  // Sorting filtered users by sortBy object
+  const sortedUsers = _.orderBy(filteredUsers, [sortBy.iter], [sortBy.order]);
+  
+  const handleProfessionSelect = (item) => setSelectedProf(item);
+
+  const handleDelete = (userId) => setUsers(users.filter(user => user._id !== userId));
+
+  const handleSort = (sortBy) => setSortBy(sortBy);
+
+  // When users and profession ready show page
+  return (sortedUsers && professions ?
+    <div className="container d-flex">
+      <div className="mt-4">
+        <GroupList items={professions} onItemSelect={handleProfessionSelect} selectedItem={selectedProf }/>
+        <button onClick={() => setSelectedProf()} className="btn btn-primary mt-3">Clear filter</button>
+      </div>
+
+      <div className="flex-grow-1 ">
+        <div className="p-4">
+          <UsersTable
+            data={sortedUsers}
+            selectedSort={sortBy}
+            onDelete={handleDelete}
+            onSort={handleSort}
+          />
+        </div>
+      </div>
+    </div> 
+    :
+    <h3 className="text-center">Loading...</h3>
+  );
+};
+
+export default UsersForm;
