ec4ba00665208e100299d24295c5bd565a7ce8dd
ainur.aimurzin <aliveairush@gmail.com>
2022-05-01 15:46:38 +0300

    Added default redirect from /fast-company to /users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/navBar";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import Main from "./layouts/mainLayout/main";
 import Users from "./layouts/usersLayout";
 import Login from "./layouts/loginLayout/login";
@@ -13,6 +13,7 @@ const App = () => {
         <Route path="/users/:userId?" component={Users} />
         <Route path="/login" component={Login} />
         <Route path="/" exact component={Main} />
+        <Redirect from="/fast-company" to="/users" />
       </Switch>
     </>
   );
